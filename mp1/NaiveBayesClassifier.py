eb153c2f77b3cc4356cf1998f42e8a717a94edeb
Norman Chung <normanchung@ucsb.edu>
2020-05-12 04:34:24 -0700

    p sure this is good

--- mp1/NaiveBayesClassifier.py
+++ mp1/NaiveBayesClassifier.py
@@ -1,11 +1,16 @@
 import sys
 import numpy
+import timeit
+
+
+start1 = timeit.default_timer()
 
 dictpos = {}
 dictneg = {}
-
 counterpos = 0
 counterneg = 0
+a = 0.01
+
 with open(sys.argv[1],'r') as file:
     for line in file:
         lastword = line.split()
@@ -29,28 +34,54 @@ with open(sys.argv[1],'r') as file:
                     counterneg += 1
 
 for eachword in dictpos:
-    dictpos[eachword]['probability'] = dictpos[eachword]['count'] / counterpos
+    dictpos[eachword]['probability'] = (dictpos[eachword]['count'] + a) / (counterpos + (a * (counterpos + counterneg)))
+
 for eachword in dictneg:
-    dictneg[eachword]['probability'] = dictneg[eachword]['count'] / counterneg
+    dictneg[eachword]['probability'] = (dictneg[eachword]['count'] + a) / (counterneg + (a * (counterpos + counterneg)))
 
-#print(dictpos['the'])
-#print(counterpos)
-#print(dictneg['the'])
-#print(counterneg)
+stop1 = timeit.default_timer()
 
-#str = 'hello this is a positive review ,1'
-#splitted = str.split()
-#splitted = splitted[0:len(splitted) - 1]
-#print(splitted)
-#print(1)
-#print('1')
+start2 = timeit.default_timer()
 
-testposcounter = 0.0
-testnegcounter = 0.0
-realposcounter = 0
-realnegcounter = 0
+realposcounter = 0.0
+realnegcounter = 0.0
+correct = 0.0
 
-#ask shaka about overfitting
+with open(sys.argv[1],'r') as file:
+    for line in file:
+        testpos = 0.0
+        testneg = 0.0
+        withoutclass = line.split()
+        classdef = withoutclass[-1]
+        if (classdef == ',1'):
+            realposcounter += 1
+        else:
+            realnegcounter += 1
+        withoutclass = withoutclass[0:len(withoutclass) - 1]
+        for eachword in withoutclass:
+            if eachword in dictpos:
+                testpos += numpy.log10(dictpos[eachword]['probability'])
+            else:
+                testpos += numpy.log10(a / (counterpos + (a * (counterpos + counterneg))))
+            if eachword in dictneg:
+                testneg += numpy.log10(dictneg[eachword]['probability'])
+            else:
+                testneg += numpy.log10(a / (counterneg + (a * (counterpos + counterneg))))
+        if (testpos > testneg):
+            #print('1')
+            if (classdef == ',1'):
+                correct += 1
+        else:
+            #print('0')
+            if (classdef == ',0'):
+                correct += 1
+
+trainingaccuracy = correct / (realposcounter + realnegcounter)
+
+realposcounter = 0.0
+realnegcounter = 0.0
+correct = 0.0
+
 with open(sys.argv[2],'r') as file:
     for line in file:
         testpos = 0.0
@@ -65,32 +96,26 @@ with open(sys.argv[2],'r') as file:
         for eachword in withoutclass:
             if eachword in dictpos:
                 testpos += numpy.log10(dictpos[eachword]['probability'])
-                #ask shaka what to do when the word is not in dictpos
+            else:
+                testpos += numpy.log10(a / (counterpos + (a * (counterpos + counterneg))))
             if eachword in dictneg:
                 testneg += numpy.log10(dictneg[eachword]['probability'])
-                #ask shaka
-        #print(testpos)
-        #print(testneg)
+            else:
+                testneg += numpy.log10(a / (counterneg + (a * (counterpos + counterneg))))
         if (testpos > testneg):
             print('1')
-            testposcounter += 1
+            if (classdef == ',1'):
+                correct += 1
         else:
             print('0')
-            testnegcounter += 1
-'''
-testnum = dictpos['each']['probability']
-test = 0.0
-test += math.log10(testnum)
-print(test)
-'''
-print('accuracy calculation now')
-total = 0.0
-total = abs(testposcounter - realposcounter)
-othertotal = abs(testnegcounter - realnegcounter)
-print(othertotal)
-print(total)
-print(realposcounter + realnegcounter)
-accuracy = total / (realposcounter + realnegcounter)
-accuracy = 1 - accuracy
-print(accuracy)
-#ask shaka his accuracy for testing.txt
+            if (classdef == ',0'):
+                correct += 1
+
+testingaccuracy = correct / (realposcounter + realnegcounter)
+
+stop2 = timeit.default_timer()
+
+print((stop1 - start1), 'seconds (training)')
+print((stop2 - start2), 'seconds (labeling)')
+print(trainingaccuracy, '(training)')
+print(testingaccuracy, '(testing)')
